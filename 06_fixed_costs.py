5a02d99fb35609d446c5dbbc4bed3baba56369db
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2020-05-26 11:59:06 +1200

    fixed costs

---
+++ 06_fixed_costs.py
@@ -0,0 +1,23 @@
+# get fixed cost ...
+fixed_costs = [
+    ["stall hire", 25.00],
+    ["artwork", 35.00],
+    ["signage", 15.00]
+]
+
+# Main routine goes here
+
+item_name=print("what is fixed cost?")
+print("Artwork")
+item_cost= print("how much does it cost?")
+print("$35.00")
+
+item_name = print("what is fixed costs ?")
+print("stall Hire")
+item_cost = print("how much does it cost?")
+print("$25.00")
+
+item_name = print("what is the fixed costs name?")
+print("signage")
+item_cost = print("how much does it cost?")
+print ("$15.00")
