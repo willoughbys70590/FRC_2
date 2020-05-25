31d40dd247d737aeb17fb5997c106ad6d6821a62
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2020-05-26 11:58:17 +1200

    states the variable and how much they cost

---
+++ 05_vairable_cost.py
@@ -0,0 +1,22 @@
+# Get variable costs...
+Variable_costs = [
+    ['Plain white Mug', 1.0],
+    ['Printing', 0.75],
+    ['Packaging', 0.5]
+]
+# Main routine goes here
+
+item_name=print("what is item name?")
+print(" packaging")
+item_cost= print("how much does your item cost?")
+print("$0.50")
+
+item_name = print("what is the item name?")
+print("plain white mug")
+item_cost = print("how much does your item cost?")
+print("$1.00")
+
+item_name = print("what is item name?")
+print("printing")
+item_cost = print("how much does your item cost?")
+print = ("$0.75")
