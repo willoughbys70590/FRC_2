58f02315fbc1332195dbd85829b3c3a35e2dce34
Stacy Willoughby <willoughbys70590@masseyhigh.school.nz>
2020-06-09 22:21:06 +0000

    fixed component five, runs once

--- 05_vairable_cost.py
+++ 05_vairable_cost.py
@@ -1,22 +1,19 @@
 # Get variable costs...
-Variable_costs = [
+variable_costs = [
     ['Plain white Mug', 1.0],
     ['Printing', 0.75],
     ['Packaging', 0.5]
 ]
-# Main routine goes here
 
-item_name=print("what is item name?")
-print(" packaging")
-item_cost= print("how much does your item cost?")
-print("$0.50")
+# main routine goes here ...
 
-item_name = print("what is the item name?")
-print("plain white mug")
-item_cost = print("how much does your item cost?")
-print("$1.00")
+variable_name = input( "what will you be making?")
+how_many = input("how many items will you be making?")
+# varible_costs = input ("please enter variable costs")
 
-item_name = print("what is item name?")
-print("printing")
-item_cost = print("how much does your item cost?")
-print = ("$0.75")
+subtotal = 0
+for item in variable_costs:
+    print("{}: ${:.2f}".format(item[0], item[1]))
+    subtotal += item[1]
+
+print(subtotal)
